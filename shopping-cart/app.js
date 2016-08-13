d3a5795e1b645f79ab5b47a045c1708d0410a6c8
amaanvaddadi <meetamaanrao@gmail.com>
2016-08-13 22:58:03 +1200

    added passport-local strategy to package and also created a passport js in config

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -7,8 +7,12 @@ var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
 var routes = require('./routes/index');
 var mongoose = require('mongoose');
-var database = require('./config/db')
-var session = require('express-session')
+var database = require('./config/db');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+
 var app = express();
 
 
@@ -27,6 +31,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
