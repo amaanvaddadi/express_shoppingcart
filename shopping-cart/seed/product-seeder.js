6ab49529682bacafcfcfa1b6d894c0cf1462961b
amaanvaddadi <meetamaanrao@gmail.com>
2016-08-18 08:44:45 +1200

    finished

--- shopping-cart/seed/product-seeder.js
+++ shopping-cart/seed/product-seeder.js
@@ -16,7 +16,7 @@ function runSeed(req,res){
     new Product({
     image: 'http://raoholdingsltd.com/files/Pro1%20(1).JPG',
     title: 'RAAO Slim Executive Wallet',
-    description: 'Tired of bulky wallets when you hardly carry 10 note bills a few coins and more cards? Does your wallet give u a bummer?? Our wallet gurus have cut the bulky crap with their Slimplify approach. After several attempts our designers have successfully created a fully functional wallet without the bulk. The Pro series would suit anyone who believes in a premium slim wallet which provides enough space to stash 4 to 8 cards, a few notes and coins.',
+    description: 'Tired of bulky wallets when you hardly carry 10 note bills a few coins and more cards? Does your wallet give u a bummer?? We have successfully created a fully functional wallet without the bulk. The Pro series would suit anyone who believes in a premium slim wallet which provides enough space to stash 4 to 8 cards, a few notes and coins.',
     price: 35
 
     }),
